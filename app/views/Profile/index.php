abbd5bbfcf86751c61686ab1c6508d40ad44360e
Christian Kfoury <christian_kfoury558@hotmail.ca>
2021-10-25 12:24:15 -0400

    Added logout to view, not implementaion. Talked about session variables, and how to use them

--- app/views/Profile/index.php
+++ app/views/Profile/index.php
@@ -5,10 +5,14 @@
 	Search for a person: <input type="text" name="searchTextbox">
 	<input type="submit" value="Search" name="search">
 </form>
+<a href="/Profile/logout">Logout</a><br>
 <a href="/Profile/editProfile">Edit Profile</a><br>
 <a href="/Profile/settings">Settings (password, 2-FA)</a><br>
 <a href="/Profile/inbox">View Inbox</a><br>
-<a href="/Profile/outbox">View Outbox</a>
+<a href="/Profile/outbox">View Outbox</a><br>
+<a href="/Profile/notifications">View notifications</a><br>
+<!-- use session variables as global variable -->
+<?php echo $_SESSION['user_id']?>
 
 <h1>Wall View</h1>
 <a href="/Profile/newPost">Make new post</a>
